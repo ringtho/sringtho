4296fb2e49147e9c68620462004ae21382e3a486
Smith Ringtho <sringtho@gmail.com>
2023-12-21 09:03:49 +0000

    Feat: Added images and caption text to the work page

--- src/pages/Work/Work.tsx
+++ src/pages/Work/Work.tsx
@@ -1,15 +1,71 @@
 import './Work.scss'
+import MovieList from '../../assets/movie.jpg'
 
 const Work = () => {
   return (
     <section className="work_container">
       <h1>Projects</h1>
-      <section className='work_wrapper'>
-        <div className="work_card">Project 1</div>
-        <div className="work_card">Project 1</div>
-        <div className="work_card">Project 1</div>
-        <div className="work_card">Project 1</div>
-        <div className="work_card">Project 1</div>
+      <section className="work_wrapper">
+        <div className="work_card">
+          <img src={MovieList} alt="movie-list" className="work_image" />
+          <div className="work_captions">
+            <div className="work_text">
+              <h1>MovieReel Database</h1>
+              <p>
+                A movie database to add movies to your watchlist, review them
+                and get external reviews
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="work_card">
+          <img src={MovieList} alt="movie-list" className="work_image" />
+          <div className="work_captions">
+            <div className="work_text">
+              <h1>ShoppingList App</h1>
+              <p>
+                A shopping list application that lets you add items, quantity
+                and description and review them.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="work_card">
+          <img src={MovieList} alt="movie-list" className="work_image" />
+          <div className="work_captions">
+            <div className="work_text">
+              <h1>ShoppingList App</h1>
+              <p>
+                A shopping list application that lets you add items, quantity
+                and description and review them.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="work_card">
+          <img src={MovieList} alt="movie-list" className="work_image" />
+          <div className="work_captions">
+            <div className="work_text">
+              <h1>ShoppingList App</h1>
+              <p>
+                A shopping list application that lets you add items, quantity
+                and description and review them.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="work_card">
+          <img src={MovieList} alt="movie-list" className="work_image" />
+          <div className="work_captions">
+            <div className="work_text">
+              <h1>ShoppingList App</h1>
+              <p>
+                A shopping list application that lets you add items, quantity
+                and description and review them.
+              </p>
+            </div>
+          </div>
+        </div>
       </section>
     </section>
   )
